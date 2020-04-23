6cdff04caba4ec4d48f89253d27724b7c2049578
its-akshara <akshara.sundar@gmail.com>
2020-04-23 11:41:28 -0700

    Swap to using symfit for data

--- sir_km.py
+++ sir_km.py
@@ -3,15 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import integrate, interpolate
-from scipy import optimize
-from scipy.odr import Model, Data, ODR
 from scipy.integrate import odeint
-    
-init_S = 999
-init_I = 1
-init_R = 0
-
+from symfit import variables, parameters, Fit, D, ODEModel
 
 def create_model_manual(beta, gamma, t, init_conds):
     S, I, R = [init_conds[0]], [init_conds[1]], [init_conds[2]]
@@ -49,22 +42,19 @@ def plot_model(model):
     plt.ylabel('Population Size')
     plt.show()
 
-class ParameterRecoverer():
-    def __init__(self, model_data, init_conds):
-        self.model_data = model_data # t, S, I, R
-        self.init_conds = init_conds
-    
-    def ode(self, y, t, beta, gamma):
-        return derivative(y, t, beta, gamma)
+def find_best_fit_params(data, init_conds):
+    S, I, R, t = variables('S, I, R, t')
+    beta, gamma = parameters('beta, gamma')
+    model_dict = {
+        D(S, t): -beta * S * I,
+        D(I, t): beta * S * I - gamma * I,
+        D(R, t): gamma * I
+    }
+    ode_model = ODEModel(model_dict, initial={t: 0.0, S: init_conds[0], I: init_conds[1], R: init_conds[2]})
+    fit = Fit(ode_model, t=np.linspace(0, 100, int(100/0.1)), I=data, S=None, R=None)
+    fit_result = fit.execute()
 
-    def model(self, t, p):
-        return odeint(self.ode, self.init_conds, t, args=(p,))
-
-    def f_resid(self, p):
-        return self.model_data - self.model(self.model_data, p)
-
-    # def best_fit_params(self, guess):
-    #     return optimize.curve_fit(self.ode, )
+    return fit_result.params
 
 def main():
     print("Enter beta.")
@@ -82,39 +72,9 @@ def main():
     init_conds = [pop_size - 1, 1, 0]
     model_data, manual = simulate_model(100, 0.1, beta, gamma, init_conds)
 
-    # param_recoverer = ParameterRecoverer(model_data.ravel(), init_conds)
-
-    # print("Estimated (beta, gamma) is: ")
-    # print(param_recoverer.best_fit_params([0.002, 0.2]))
-
-    # def SIR_ODE(params, t):
-    #     beta, gamma = params
-    
-    #     def deriv(y, t):
-    #         S, I, R = y
-    #         dSdt = - beta * S * I
-    #         dIdt = beta * S * I - gamma * I
-    #         dRdt = gamma * I
-    #         return [dSdt, dIdt, dRdt]
-
-    #     y = odeint(deriv, init_conds, t)
-    #     return y[:,1]
-
-    # print(np.shape(model_data))
-    # t = np.linspace(0, 100, int(100/0.1))
-    # # data = Data(np.repeat(t, 3), model_data)
-    # data = Data(t, model_data)
-    # model = Model(SIR_ODE)
-    # guess = [0.002, 0.2]
-    # odr = ODR(data, model, guess)
-    # odr.set_job(2)
-    # out = odr.run()
-    # f = plt.figure()
-    # p = f.add_subplot(111)
-    # p.plot(t, model_data)
-    # print(out.beta)
-    # p.plot(t, SIR_ODE(out.beta, t))
-    # plt.show()
+    print(np.shape(np.ndarray.transpose(manual)))
+    transposed = np.ndarray.transpose(manual)
+    print(find_best_fit_params(transposed[1], init_conds))
 
 if __name__ == "__main__":
     main()
